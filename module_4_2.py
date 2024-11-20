1b6aa562bec4ea4f14d720d4fa7ab3ec3a698744
Smyyug <100402danil@gmail.com>
2024-11-20 17:20:45 +0300

    Add files via upload

--- module_4_2.py
+++ module_4_2.py
@@ -3,6 +3,6 @@ def test_function():
         return "Я в области видимости функции test_function"
     a = inner_function()
     print(a)
-test_function()
-inner_function()
+test_function() # выводит Я в области видимости функции test_function
+inner_function() # выводит ошибку, так как не видит функцию inner_function
 
